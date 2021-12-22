c510ef236a1763d0a155fa65a50589ef168b947f
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-22 20:03:36 +0000

    Destructuring introduction

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,30 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27]
+// let john = ages[0];
+// let mary = ages[1]
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe)
 
 // Destructuring objects
-
+let jobs = {
+    mike: 'designer',
+    jill: 'developer',
+    alicia: 'accountant'
+};
+let {
+    mike,
+    jill,
+    alicia
+} = jobs;
+console.log(mike, jill, alicia);
 // Destructuring subsets
 
+let languages = ['english', 'french', 'spanish', 'german',
+    'japanese'
+];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
 // Using rest parameter syntax
