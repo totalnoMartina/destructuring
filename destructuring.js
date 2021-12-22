9dc6cd12466ba7b131eef12f9cbce0c716a7f2f6
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-22 20:17:03 +0000

    Desctructuring practice

--- destructuring.js
+++ destructuring.js
@@ -30,4 +30,44 @@ let languages = ['english', 'french', 'spanish', 'german',
 ];
 let [johnNative, johnSecondary] = languages;
 console.log(johnNative, johnSecondary);
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: 'english',
+    secondLanguage: 'french',
+    thirdLanguage: 'german',
+    fourthLanguage: 'japanese'
+};
+
+let {
+    firstLanguage,
+    thirdLanguage
+} = languages2;
+
+console.log(firstLanguage, thirdLanguage);
 // Using rest parameter syntax
+
+let fruits = ['apple', 'orange', 'banana', 'peach', 'cherry'];
+let [favorite, secondFavorite, ...others] = fruits;
+console.log(favorite);
+console.log(secondFavorite);
+console.log(others);
+
+let favoriteFoods = {
+    brian: 'pizza',
+    anna: 'pasta',
+    sarah: 'vegetables',
+    andrea: 'steak'
+
+};
+let {
+    brian,
+    anna,
+    ...rest
+} = favoriteFoods;
+
+console.log(brian);
+console.log(anna);
+console.log(rest);
